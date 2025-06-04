bcbfbfa29460a947d786ce014bab79298b9cd48f
Thiago Antonio <thiagoamcmacedo1@gmail.com>
2025-06-04 15:20:38 -0300

    Outros métodos facilitadores de arrays

---
+++ 4.6 Other array methods/script.js
@@ -0,0 +1,13 @@
+//includes(), startsWith(), endsWith()
+
+let nome = ['maucio', 'fontres', 'suiwelen']
+
+if (nome.includes("Maria")) {
+    console.log("SIM")
+}
+else {
+    console.log('NÃO')
+}
+
+console.log(nome[0].startsWith("mau"))
+console.log(nome[0].endsWith("cio"))
